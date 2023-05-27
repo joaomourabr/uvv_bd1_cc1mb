475b1738bd492226cf5f99d1caedae1a435697ab
João Moura <69054684+joaomourabr@users.noreply.github.com>
2023-05-27 17:18:09 -0300

    Update cc1mb_202307793_postgresql.sql

--- PSET1/cc1mb_202307793_postgresql.sql
+++ PSET1/cc1mb_202307793_postgresql.sql
@@ -233,7 +233,7 @@ NOT DEFERRABLE;
 
 
 ALTER TABLE lojas
-    ADD CONSTRAINT endereco_check CHECK (COALESCE (endereco_web, '' ) <> '' OR COALESCE(endereco_fisico, '') <> '');
+    ADD CONSTRAINT endereco_check CHECK (endereco_web IS NOT NULL OR endereco_fisico IS NOT NULL);
  
 ALTER TABLE pedidos
     ADD CONSTRAINT status_pedido_check CHECK (status IN ('CANCELADO', 'COMPLETO', 'ABERTO', 'PAGO', 'REEMBOLSADO', 'ENVIADO'));
